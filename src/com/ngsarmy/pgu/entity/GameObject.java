7735601c5407f0dd2e218c7ed6657ff7b1ad25db
Apaar Madan <goodpaul6@hotmail.com>
2014-05-06 07:35:04 -0400

    added a bunch of useful collision methods

--- src/com/ngsarmy/pgu/entity/GameObject.java
+++ src/com/ngsarmy/pgu/entity/GameObject.java
@@ -1,18 +1,135 @@
 package com.ngsarmy.pgu.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ngsarmy.pgu.core.GameEvent;
+import com.ngsarmy.pgu.core.GameRasterizer;
 import com.ngsarmy.pgu.core.GameState;
 import com.ngsarmy.pgu.utils.Rectangle;
+import com.ngsarmy.pgu.utils.Vector2;
 
 public class GameObject 
 {
 	private GameState state;
-	protected Rectangle rectangle;
-	protected String name;
-	protected String type;
-	protected int layer;
-	protected boolean collidable;
+	public Rectangle rectangle;
+	public String name;
+	public String type;
+	public int layer;
+	public boolean collidable;
 	
-	public GameObject()
+	public GameObject(Vector2 position, String type)
 	{
+		rectangle = new Rectangle(position.x, position.y, 0, 0);
+		name = "default";
+		this.type = type;
+		layer = 0;
+		collidable = true;
+	}
+
+	public void event(GameEvent ev)
+	{
+	}
+	
+	public void update(double delta)
+	{
+	}
+	
+	public void render(GameRasterizer g)
+	{
+	}
+	
+	public Vector2 getPos()
+	{
+		return rectangle.position;
+	}
+	
+	public float getTop()
+	{
+		return rectangle.position.y;
+	}
+	
+	public float getLeft()
+	{
+		return rectangle.position.x;
+	}
+	
+	public float getBottom()
+	{
+		return rectangle.position.y + rectangle.size.y;
+	}
+	
+	public float getRight()
+	{
+		return rectangle.position.x + rectangle.size.x;
+	}
+	
+	protected GameObject collide(float x, float y, String type)
+	{
+		List<GameObject> objs = new ArrayList<GameObject>();
+		state.getObjectsWithType(type, objs);
+		
+		Rectangle temp = new Rectangle(rectangle.position.x + x, rectangle.position.y + y, rectangle.size.x, rectangle.size.y);
+		
+		for(int i = 0; i < objs.size(); i++)
+		{
+			GameObject go = objs.get(i);
+			
+			if(go.rectangle.collide(temp))
+				return go;
+		}
+		
+		return null;
+	}
+	
+	protected GameObject collideTypes(float x, float y, String[] types)
+	{
+		List<GameObject> objs = new ArrayList<GameObject>();
+		for(int i = 0; i < types.length; i++)
+			state.getObjectsWithType(types[i], objs);
+		
+		Rectangle temp = new Rectangle(rectangle.position.x + x, rectangle.position.y + y, rectangle.size.x, rectangle.size.y);
+		
+		for(int i = 0; i < objs.size(); i++)
+		{
+			GameObject go = objs.get(i);
+			
+			if(go.rectangle.collide(temp))
+				return go;
+		}
+		return null;
+	}
+	
+	protected void collideInto(float x, float y, String type, List<GameObject> objs)
+	{
+		List<GameObject> possible = new ArrayList<GameObject>();
+		state.getObjectsWithType(type, possible);
+		
+		Rectangle temp = new Rectangle(rectangle.position.x + x, rectangle.position.y + y, rectangle.size.x, rectangle.size.y);
+		
+		for(int i = 0; i < possible.size(); i++)
+		{
+			GameObject go = possible.get(i);
+			
+			if(go.rectangle.collide(temp))
+				objs.add(go);
+		}
+	}
+	
+	protected void collideTypesInto(float x, float y, String[] types, List<GameObject> objs)
+	{
+		List<GameObject> possible = new ArrayList<GameObject>();
+		for(int i = 0; i < types.length; i++)
+			state.getObjectsWithType(types[i], possible);
+		
+		Rectangle temp = new Rectangle(rectangle.position.x + x, rectangle.position.y + y, rectangle.size.x, rectangle.size.y);
+		
+		for(int i = 0; i < possible.size(); i++)
+		{
+			GameObject go = possible.get(i);
+			
+			if(go.rectangle.collide(temp))
+				objs.add(go);
+		}
 	}
 }
--- src/com/ngsarmy/pgu/entity/GameObjectManager.java
+++ src/com/ngsarmy/pgu/entity/GameObjectManager.java
@@ -1,5 +1,119 @@
 package com.ngsarmy.pgu.entity;
 
-public class GameObjectManager {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.ngsarmy.pgu.core.GameEvent;
+import com.ngsarmy.pgu.core.GameRasterizer;
+
+public class GameObjectManager 
+{
+	static enum Action
+	{
+		ADD_GO,
+		REM_GO
+	}
+	
+	static class PendingChange
+	{
+		Action action;
+		GameObject ent;
+		
+		public PendingChange(Action action, GameObject ent) 
+		{
+			this.action = action;
+			this.ent = ent;
+		}
+	}
+	
+	private List<GameObject> objectList;
+	private List<PendingChange> pendingChangeList;
+	
+	public GameObjectManager()
+	{
+		objectList = new ArrayList<GameObject>();
+	}
+	
+	public void event(GameEvent ev)
+	{
+		for(int i = 0; i < objectList.size(); i++)
+			objectList.get(i).event(ev);
+		
+		applyPendingChanges();
+	}
+	
+	public void update(double delta)
+	{
+		for(int i = 0; i < objectList.size(); i++)
+			objectList.get(i).update(delta);
+		
+		applyPendingChanges();
+	}
+	
+	public void render(GameRasterizer g)
+	{
+		for(int i = 0; i < objectList.size(); i++)
+			objectList.get(i).render(g);
+	}
+	
+	public GameObject add(GameObject object)
+	{
+		pendingChangeList.add(new PendingChange(Action.ADD_GO, object));
+		return object;
+	}
+	
+	public void remove(GameObject object)
+	{
+		pendingChangeList.add(new PendingChange(Action.REM_GO, object));
+	}
+	
+	public GameObject getObjectWithName(String name)
+	{
+		for(int i = 0; i < objectList.size(); i++)
+		{
+			GameObject go = objectList.get(i);
+			
+			if(go.name.equals(name))
+				return go;
+		}
+		return null;
+	}
+	
+	public void getObjectsWithType(String type, List<GameObject> objs)
+	{
+		for(int i = 0; i < objectList.size(); i++)
+		{
+			GameObject go = objectList.get(i);
+			if(go.type.equals(type))
+				objs.add(go);
+		}
+	}
+	
+	public void getObjectsWithLayer(int layer, List<GameObject> objs)
+	{
+		for(int i = 0; i < objectList.size(); i++)
+		{
+			GameObject go = objectList.get(i);
+			if(go.layer == layer)
+				objs.add(go);
+		}
+	}
+	
+	private void applyPendingChanges()
+	{
+		for(int i = 0; i < pendingChangeList.size(); i++)
+		{
+			PendingChange change = pendingChangeList.get(i);
+			
+			switch(change.action)
+			{
+			case ADD_GO:
+				objectList.add(change.ent);
+				break;
+			case REM_GO:
+				objectList.remove(change.ent);
+				break;
+			}
+		}
+	}
 }
